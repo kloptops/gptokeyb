04248c601dc03cfbe106a6286138d9f96ef44462
Jacob Smith <kloptops@gmail.com>
2023-01-30 19:31:31 +0800

    Initial restructuring.

---
+++ src/input.cpp
@@ -0,0 +1,39 @@
+/* Copyright (c) 2021-2023
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+#
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* General Public License for more details.
+#
+* You should have received a copy of the GNU General Public
+* License along with this program; if not, write to the
+* Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301 USA
+#
+* Authored by: Kris Henriksen <krishenriksen.work@gmail.com>
+#
+* AnberPorts-Keyboard-Mouse
+* 
+* Part of the code is from from https://github.com/krishenriksen/AnberPorts/blob/master/AnberPorts-Keyboard-Mouse/main.c (mostly the fake keyboard)
+* Fake Xbox code from: https://github.com/Emanem/js2xbox
+* 
+* Modified (badly) by: Shanti Gilbert for EmuELEC
+* Modified further by: Nikolai Wuttke for EmuELEC (Added support for SDL and the SDLGameControllerdb.txt)
+* Modified further by: Jacob Smith
+* 
+* Any help improving this code would be greatly appreciated! 
+* 
+* DONE: Xbox360 mode: Fix triggers so that they report from 0 to 255 like real Xbox triggers
+*       Xbox360 mode: Figure out why the axis are not correctly labeled?  SDL_CONTROLLER_AXIS_RIGHTX / SDL_CONTROLLER_AXIS_RIGHTY / SDL_CONTROLLER_AXIS_TRIGGERLEFT / SDL_CONTROLLER_AXIS_TRIGGERRIGHT
+*       Keyboard mode: Add a config file option to load mappings from.
+*       add L2/R2 triggers
+* 
+* Spaghetti code incoming, beware :)
+*/
+
+#include "gptokeyb.h"
